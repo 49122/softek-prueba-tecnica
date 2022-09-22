6ff4b86b34c642d2ac2bebf02800d62c2c3ea840
Jorge Alberto Delgadillo Alonso <git config --global user.email you@example.com>
2022-09-21 20:02:35 -0500

    ADD: added resource 3 test and more documentation

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,7 +1,9 @@
 """Testing module using pytest"""
 from validations.data_validations import customer_order_status_data_validation
 from validations.data_validations import seasons_data_validation
+from validations.data_validations import detecting_change_data_validation
 from validations.seasons_validations import get_season
+from validations.detect_weather_change import sort_dates,change_detection
 
 valid_data_customer_order_status = [
        [ "ORD_1567", "LAPTOP", "SHIPPED"],
@@ -28,7 +30,6 @@ not_valid_data_str_customer_order_status = [
        [ "ORD_7654", "TV", "CANCELLED"],
        [ "ORD_7654", "DVD", "CANCELLED"]
     ]
-
 
 not_valid_data_len_customer_order_status = [
        [ "ORD_1567", "LAPTOP", "SHIPPED"],
@@ -89,6 +90,45 @@ not_valid_data_date_seasons = [
        [ "114-7232801-4607440", "10/9/20", 1]
     ]
 
+valid_data_changes = [
+    ["1/1/20", "FALSE"],
+    ["1/2/20", "TRUE"],
+    ["1/3/20", "TRUE"],
+    ["1/4/20", "FALSE"],
+    ["1/5/20", "FALSE"],
+    ["1/6/20", "TRUE"],
+    ["1/7/20", "FALSE"],
+    ["1/8/20", "TRUE"],
+    ["1/9/20", "TRUE"],
+    ["1/10/20", "TRUE"]
+    ]
+
+not_valid_data_date_changes = [
+    ["1/1/20", "FALSE"],
+    ["1/2/20", "TRUE"],
+    ["1/3/20", "TRUE"],
+    ["13/4/20", "FALSE"],
+    ["1/5/20", "FALSE"],
+    ["1/6/20", "TRUE"],
+    ["1/7/20", "FALSE"],
+    ["1/8/20", "TRUE"],
+    ["1/9/20", "TRUE"],
+    ["1/10/20", "TRUE"]
+    ]
+
+not_valid_data_len_changes = [
+    ["1/1/20", "FALSE",1],
+    ["1/2/20", "TRUE"],
+    ["1/3/20", "TRUE"],
+    ["13/4/20", "FALSE"],
+    ["1/5/20", "FALSE"],
+    ["1/6/20", "TRUE"],
+    ["1/7/20", "FALSE"],
+    ["1/8/20", "TRUE"],
+    ["1/9/20", "TRUE"],
+    ["1/10/20", "TRUE"]
+    ]
+
 """
 For the following test we'll be testing:
 "Given" different types of data
@@ -132,3 +172,38 @@ def test_not_valid_date():
     """Test to validate the correct date format"""
 
     assert seasons_data_validation(not_valid_data_date_seasons) is False
+
+def test_getting_season():
+    """Test to validate the correct date format"""
+
+    assert get_season("1/2/20") == "Winter"
+
+# Detecting changes
+def test_valid_data_changes():
+    """Test to validate the correct functioning of our data validator"""
+
+    assert detecting_change_data_validation(valid_data_changes) is True
+
+def test_not_valid_data_date_format_changes():
+    """Test to validate the correct functioning of our data validator"""
+
+    assert detecting_change_data_validation(not_valid_data_date_changes) is False
+
+def test_not_valid_data_len_changes():
+    """Test to validate the correct functioning of our data validator"""
+
+    assert detecting_change_data_validation(not_valid_data_len_changes) is False
+
+def test_sorting_dates_changes():
+    """Test to validate the correct functioning of our data sorting script"""
+
+    assert sort_dates([
+    ["1/1/20", "FALSE"],
+    ["1/2/20", "TRUE"],
+    ["1/3/20", "TRUE"],]) == [["1/1/20", "FALSE"],["1/2/20", "TRUE"],["1/3/20", "TRUE"]]
+
+def test_change_detection():
+    """Test to validate the correct functioning our change detector"""
+
+    assert change_detection(valid_data_changes) == [
+        ["1/2/20","TRUE"],["1/6/20","TRUE"],["1/8/20","TRUE"]]
